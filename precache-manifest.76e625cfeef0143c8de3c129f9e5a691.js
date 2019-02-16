1fd296ec4e5533d8a7bef4edb85f851d9b3f91bd
surasnayak <surasnayak@gmail.com>
2019-02-16 16:27:03 +0530

    Updates

---
+++ precache-manifest.76e625cfeef0143c8de3c129f9e5a691.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "76774577dd1b7b3689ed153fdfdc23e3",
+    "url": "/clickies/static/media/image2.76774577.jpg"
+  },
+  {
+    "revision": "fe58bf2f4dc9055a679e",
+    "url": "/clickies/static/css/main.67dcfc3f.chunk.css"
+  },
+  {
+    "revision": "ad67894f4fda9916978c",
+    "url": "/clickies/static/js/runtime~main.ad67894f.js"
+  },
+  {
+    "revision": "3fd41b06815c130bf8f31d7414181b05",
+    "url": "/clickies/static/media/surasnayak.3fd41b06.jpg"
+  },
+  {
+    "revision": "8f63eec3cde36cf3537e",
+    "url": "/clickies/static/js/2.8f63eec3.chunk.js"
+  },
+  {
+    "revision": "6fdb3e70f8147a0f7101fc6f93a58419",
+    "url": "/clickies/static/media/image1.6fdb3e70.jpg"
+  },
+  {
+    "revision": "fe58bf2f4dc9055a679e",
+    "url": "/clickies/static/js/main.fe58bf2f.chunk.js"
+  },
+  {
+    "revision": "c3dcfcf14b023bd822abbbdd0b7f805d",
+    "url": "/clickies/static/media/image3.c3dcfcf1.jpg"
+  },
+  {
+    "revision": "4a322409d9c1b0cd818d0154ec8f0895",
+    "url": "/clickies/static/media/image4.4a322409.jpg"
+  },
+  {
+    "revision": "cac3c4224eb9295cdb4de3983b7b8120",
+    "url": "/clickies/static/media/image5.cac3c422.jpg"
+  },
+  {
+    "revision": "1eab898c065018f922bbe32f78c3af0a",
+    "url": "/clickies/static/media/sumanth.1eab898c.jpg"
+  },
+  {
+    "revision": "8f63eec3cde36cf3537e",
+    "url": "/clickies/static/css/2.b962b8ed.chunk.css"
+  },
+  {
+    "revision": "378d64d28efcd16b77ed9b56756de46a",
+    "url": "/clickies/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/clickies/precache-manifest.76e625cfeef0143c8de3c129f9e5a691.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/clickies/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
