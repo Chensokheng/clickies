41a578c5bb59a42f1e8192ed401bbfd392f03166
Chensokheng <chensokheng18@kit.edu.kh>
2019-02-19 12:40:17 +0700

    make the navbar smooth transiton

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Navigation.css';
-
 const Navigation = () => {
 	return (
 		<nav>
-		 	<ul className="list mt0 mb0 pt3 pb3 bg-light-red white fw6 flex o-70 ttu tracked">
-		 		<a className="link white" href="#"><li className="ml3">HOME</li></a>
-		 		<a className="link white" href="#gallery"><li className="ml3">Gallery</li></a>
-		 		<a className="link white" href="#contributors"><li className="ml3">Contributors</li></a>
-		 		<a className="link white ml-auto" href="#"><li className="ml-auto mr4">Contact</li></a>
-		 	</ul>
+				 <a href='#' className='bttn-dark'>Home</a>
+				 <a href='#gallery'className='changeColor'>Gallery</a>
+				 <a href='#contributors'className='changeColor'>Contributors</a>
+				 <a href='#' className='contact changeColor'>Contact</a>
 		</nav>
 	);
 }
